88b7384b15af44d3fd8a947ada917cf308bff7be
Edinsson Melo <edinssonadrianmc@ufps.edu.co>
2021-06-07 22:35:07 -0500

    Fixed clase conexion

--- src/main/java/co/ufps/elecciones/utils/Conexion.java
+++ src/main/java/co/ufps/elecciones/utils/Conexion.java
@@ -3,24 +3,32 @@ package co.ufps.elecciones.utils;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-public class Conexion {
-		private EntityManager manager = null;
-		
-		public Conexion() {
-		}
-		
-		
-		public void setManager(EntityManager manager) {
-			this.manager = manager;
-		}
+import javax.persistence.TypedQuery;
 
 
-		public EntityManager getManager(){
-			if ( manager == null ) {
-				EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistencia");
-	            this.setManager(emf.createEntityManager());
-	        }
-			return manager;
-		}
+public class Conexion<T> {
+	private Class<T> c;
+	private static EntityManager em = null;
+	
+	public Conexion() {
+		em = this.getManager();
+	}
+	
+	public Conexion(Class<T> c) {
+		em = this.getManager();
+		this.c = c;
+	}
+	
+	public void setC(Class<T> c){
+		this.c = c;
+	}
+	
+	public static EntityManager getManager(){
+		if ( em == null ) {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistencia");
+            em = emf.createEntityManager();
+        }
+		return em;
+	}
+
 }
